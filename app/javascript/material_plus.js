7deca4c59f31b8010ddda7750361d5ce5951fdd2
gaku1114 <ga1114ku@gmail.com>
2023-02-17 03:42:50 +0900

    材料追加フォームをエンターキーで送信させる

--- app/javascript/material_plus.js
+++ app/javascript/material_plus.js
@@ -3,8 +3,7 @@ function plus () {
     if(e.target.closest("#material-plus")) {
       const formSpace = document.getElementById("materials-top");
       const materialFormHtml = `<form class="new-material-form" action="/dishes/${gon.dish_id}/materials" accept-charset="UTF-8" method="post">
-                                  <input type="hidden" name="authenticity_token" value="qtfuCBtXVtsiOESiA7f3FwhO4r1uLcfl0Q3gfkNFJt+0vczaMybTzv1WMwoF1HBRkFYe2XtQwuMfLeUI2tQZ1w==">
-                                  <textarea class="material-new-name" placeholder="材料を追加する" name="material[name]" id="material_name"></textarea>
+                                  <input type="text" class="material-new-name" placeholder="材料を追加する"  id="material_name" name="material[name]">
                                   <input type="submit" name="commit" value="追加" class="material-new-btn" data-disable-with="追加">
                                 </form>`;
       formSpace.innerHTML = materialFormHtml;
