8d0f43286b1cb6315cdc9dda5a38d5bebe42b974
gaku1114 <ga1114ku@gmail.com>
2023-02-18 06:10:42 +0900

    autofocus: true

--- app/javascript/material_plus.js
+++ app/javascript/material_plus.js
@@ -3,7 +3,7 @@ function plus () {
     if(e.target.closest(".material-plus")) {
       const formSpace = document.getElementById("materials-top");
       const materialFormHtml = `<form class="new-material-form" action="/dishes/${gon.dish_id}/materials" accept-charset="UTF-8" method="post">
-                                  <input type="text" class="material-new-name" placeholder="材料を追加する"  id="material_name" name="material[name]">
+                                  <input type="text" class="material-new-name" placeholder="材料を追加する"  id="material_name" name="material[name]" autofocus="autofocus">
                                   <input type="submit" name="commit" value="追加" class="material-new-btn" data-disable-with="追加">
                                 </form>`;
       formSpace.innerHTML = materialFormHtml;
