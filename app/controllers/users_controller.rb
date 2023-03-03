51afc6f416a691d28c24d60e5ae9859b025af84e
gaku1114 <ga1114ku@gmail.com>
2023-03-04 00:58:54 +0900

    マイページ編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @dishes = Dish.where(user_id: @user.id)
+    @genre_ranks = @user.genres.find(@user.dishes.group(:genre_id).order('count(genre_id) desc').pluck(:genre_id))
+    binding.pry
   end
 end
