8df5c8664eb75744d1f3b53c16493e8c19659d8a
gaku1114 <ga1114ku@gmail.com>
2023-03-04 15:49:41 +0900

    マイページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @dishes = Dish.where(user_id: @user.id)
     @genre_ranks = @user.genres.find(@user.dishes.group(:genre_id).order('count(genre_id) desc').pluck(:genre_id))
-    binding.pry
+    @materials = Material.where(dish_id: @dishes.ids)
+    @material_ranks = @materials.group(:name).order('count_name desc').count(:name).keys
   end
 end
