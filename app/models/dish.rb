a6b188fd4f29a22da24c5fb81f0faa9c1acd0997
gaku1114 <ga1114ku@gmail.com>
2023-03-04 21:13:04 +0900

    画像の拡張子のバリデーションを設定

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,9 +1,19 @@
 class Dish < ApplicationRecord
   validates :dish_name, presence: true
   validates :user_id,   presence: true
+  validate :image_content_type, if: :was_attached?
   
   belongs_to :user
   belongs_to :genre, optional: true
   has_many :materials, dependent: :destroy
   has_one_attached :image
+
+  def image_content_type
+    extension = ['image/png', 'image/jpg', 'image/jpeg']
+    errors.add(:image, " この画像は保存できません") unless image.content_type.in?(extension)
+  end
+
+  def was_attached?
+    self.image.attached?
+  end
 end
