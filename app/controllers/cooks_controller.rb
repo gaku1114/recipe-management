dbe609b4ff7adfdd59df6e89c6ee403fdb330902
gaku1114 <ga1114ku@gmail.com>
2023-03-07 22:21:36 +0900

    修正

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -2,21 +2,19 @@ class CooksController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @cook = Cook.new
     @dish = Dish.find(params[:dish_id])
     unless @dish.user == current_user
       redirect_to dishes_path
     end
-
-    @cook = Cook.new
-    session[:previous_url] = request.referer 
   end
 
   def create
     @cook = Cook.new(cook_params)
     if @cook.save
-      redirect_to session[:previous_url]
+      redirect_to detail_dish_path(params[:dish_id])
     else
-      render :new
+      redirect_to action: :new
     end
   end
 
