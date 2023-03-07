3e759c754fd6ede31fc8b0abb7539b785136778e
gaku1114 <ga1114ku@gmail.com>
2023-03-07 22:25:17 +0900

    エラーメッセージ日本語

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -11,7 +11,7 @@ class Dish < ApplicationRecord
 
   def image_content_type
     extension = ['image/png', 'image/jpg', 'image/jpeg']
-    errors.add(:image, " この画像は保存できません") unless image.content_type.in?(extension)
+    errors.add(:image, "は保存できません") unless image.content_type.in?(extension)
   end
 
   def was_attached?
